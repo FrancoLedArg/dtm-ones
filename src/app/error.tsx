74c4df5b3a2badba5644e2ffe6ed7a0294745be9
FrancoLedesma <francoledesma100@gmail.com>
2026-01-21 18:49:07 -0300

    A few more helper pages

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  return (
+    <div>
+      <h1>Something went wrong</h1>
+      <p>{error.message}</p>
+    </div>
+  );
+}
---
+++ src/app/global-error.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function GlobalError({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  return (
+    <html>
+      <body>
+        <h1>Something went wrong</h1>
+        <p>{error.message}</p>
+      </body>
+    </html>
+  );
+}
